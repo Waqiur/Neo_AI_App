53aebb0b76614f67411167a7050914eb4909d612
Vanraj Pardeshi <87256781+Vanraj8169@users.noreply.github.com>
2023-10-30 21:44:47 +0530

    Added Contribution.dart
    Hey @Waqiur 
    I have added Contribution. dart file to the repository.
    Please approve and merge my pull request.
    Thanking  You!

---
+++ lib/Contribution.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Hello, Flutter App'),
+        ),
+        body: Center(
+          child: Text(
+            'Hello, Flutter',
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
